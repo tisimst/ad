657772538ad6d6bba634dde000cd86a9bea28835
tisimst <tisimst@gmail.com>
2015-04-17 13:42:50 -0600

    incremented version to 1.3.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ readme = 'README'
 
 distutils.core.setup(
     name='ad',
-    version='1.3',
+    version='1.3.1',
     author='Abraham Lee',
     author_email='tisimst@gmail.com',
     description='Fast, transparent first- and second-order automatic differentiation',
